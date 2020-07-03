2b4583281ee6c658c162b998bddacbe0b97772c3
jack <biffisbuffm@gmail.com>
2020-07-03 11:05:52 -0500

    fixed column heights

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// search for city
+//dynamically create 'cityName' div and append to search container
+//create div with temp, humidity, widspeed, and uv index values for selected city
+// create 5 card divs and append to right hand column; each care with date, icon of weather, temp, humidity
+// localStorage key-value pair of city & weather results(jsonified)
