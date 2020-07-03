3972c7eed322cecf6c6a67b9ec14cb0efec211c7
jack <biffisbuffm@gmail.com>
2020-07-03 13:18:17 -0500

    function calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,31 @@
 // //var cityFormEl = document.querySelector("#city-form");
 var cityEntryEl = document.querySelector("#form-control");
 
-// when search button is clicked
+
+var getWeather = function(cityID) {
+    var key = '{b2fb04acd10970e7fa65712d42f4e333}';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)
+    .then(function(response) {
+        return response.json()})
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function() {
+            //catch any errors
+        });
+    }
+
+  
+
+// when search button is clicked, search or alert
+// section works//
 var getCity = function(event) {
     var cityName = cityEntryEl.value.trim();
     if (cityName) {
         getWeather(cityName);
-        cityEntyEl.value="";
+        
     } else {
+        // if form-control is empty
         alert("Please enter a city");
     }
     
@@ -19,7 +37,6 @@ var getCity = function(event) {
 // append variableto col-4
 
 
-//dynamically create 'cityName' div and append to search container
 //create div with temp, humidity, widspeed, and uv index values for selected city
 // create 5 card divs and append to right hand column; each care with date, icon of weather, temp, humidity
 // localStorage key-value pair of city & weather results(jsonified)
